fecd08987bf7203c0033db840b42ed7d2d9a7822
obeconene381-cmyk <obeconene381@gmail.com>
2026-06-23 18:30:39 +0100

    Update automation.py

--- automation.py
+++ automation.py
@@ -9,7 +9,7 @@ import json
 import base64
 from playwright.async_api import async_playwright
 
-=======================
+#=======================
 # الإعدادات - تُقرأ من متغيرات البيئة
 # ==========================================
 BOT_TOKEN = os.environ.get("BOT_TOKEN", os.environ.get("TELEGRAM_BOT_TOKEN", ""))
